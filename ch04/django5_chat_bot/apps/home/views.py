473557e8955d989ea592e61e669d242e2006efed
大鹏 <pygosuperman@outlook.com>
2026-03-03 11:55:47 +0800

    整合AI聊天

--- ch04/django5_chat_bot/apps/home/views.py
+++ ch04/django5_chat_bot/apps/home/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-
+from apps.chatbot.openai_client import client, MODEL
 
 def home(request):
     """首页视图函数"""
-    return render(request, 'home/index.html')
+    # 拿到传递过来的用户消息
+    message = request.POST.get('message', '')
+    answer = ""
+
+    # 用户消息非空
+    if message:
+        # 进行AI聊天
+        messages = [
+            {"role": "system", "content": "你是有个幽默有趣的聊天机器人"},
+            {"role": "user", "content": message}
+        ]
+
+        # 创建聊天
+        completion = client.chat.completions.create(
+            # 模型名称
+            model=MODEL,
+            # 聊天内容
+            messages=messages,
+        )
+        answer = completion.choices[0].message.content
+
+    # 传递个网页的消息
+    context = {
+        "answer": answer
+    }
+    return render(request, 'home/index.html', context)
