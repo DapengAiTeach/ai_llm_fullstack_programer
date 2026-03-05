f266d6740b2062ac6ebd3db391461505e673fc2f
大鹏 <pygosuperman@outlook.com>
2026-03-05 18:42:41 +0800

    实现图片上传并预览核心功能

--- ch07/django_image_tool/apps/image_tool/views.py
+++ ch07/django_image_tool/apps/image_tool/views.py
@@ -1,4 +1,17 @@
+import base64
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'image_tool/index.html')
+    image_data = None
+    if request.method == 'POST' and request.FILES.get('image'):
+        image = request.FILES['image']
+        # 读取文件内容并转为 base64
+        image_b64 = base64.b64encode(image.read()).decode()
+        # 获取文件类型
+        content_type = image.content_type
+        image_data = f'data:{content_type};base64,{image_b64}'
+    context = {
+        'image_data': image_data
+    }
+    return render(request, 'image_tool/index.html', context)
