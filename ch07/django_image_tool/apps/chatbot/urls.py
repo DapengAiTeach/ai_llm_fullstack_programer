aaaf7493a2f6b93721c239f22146facb644360b9
大鹏 <pygosuperman@outlook.com>
2026-03-05 18:11:30 +0800

    创建AI聊天首页

--- ch07/django_image_tool/apps/chatbot/urls.py
+++ ch07/django_image_tool/apps/chatbot/urls.py
@@ -1,5 +1,11 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
+# 应用名称
+app_name = "chatbot"
+
+# 路由列表
 urlpatterns = [
+    # 聊天机器人首页
+    path('', views.index, name='index'),
 ]
