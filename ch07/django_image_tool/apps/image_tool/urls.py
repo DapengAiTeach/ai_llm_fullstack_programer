9e54efaf836f902ab2d3e77425acbb56b4fef02e
大鹏 <pygosuperman@outlook.com>
2026-03-05 18:34:36 +0800

    创建图片工具首页

--- ch07/django_image_tool/apps/image_tool/urls.py
+++ ch07/django_image_tool/apps/image_tool/urls.py
@@ -1,5 +1,10 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
+# 应用名称
+app_name = "image_tool"
+
+# 路由列表
 urlpatterns = [
+    path('', views.index, name='index'),
 ]
